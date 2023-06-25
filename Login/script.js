f91eb44f82234097512f99c0d573817b0086d4be
kofikrvmo <79403677+kofikrvmo@users.noreply.github.com>
2023-06-25 03:04:14 +0000

    Add files via upload

---
+++ Login/script.js
@@ -0,0 +1,13 @@
+function login() {
+    e.preventDefault(); // Prevent form submission
+
+    // Get form values
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+
+    // Perform login validation
+    // Replace this with your own login logic
+}if (email === "user@example.com" && password === "password") {
+        alert("Login successful!");
+    }
+
